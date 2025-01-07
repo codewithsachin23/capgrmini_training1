6cf39c2836c94ff3fb7417cf0d9b99a43fb4ad17
codewithsachin23 <144677889+codewithsachin23@users.noreply.github.com>
2025-01-07 15:22:47 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -1,29 +1,21 @@
 class Calculator{
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 	void add(int a, int b){
 		System.out.println(a+b);
 	}
->>>>>>> add
-=======
+
 	void sub(int a, int b){
 		System.out.println(a-b);
 	}
->>>>>>> sub
-=======
+
 	void multiply(int a,int b){
 		System.out.println(a*b);
 }
->>>>>>> multiply
 
-=======
 	void devide(int a,int b){
 		System.out.println(a/b);
 }
->>>>>>> devide
+
 	public static void main(String args[]){
 		int a = 10, b = 5;
 		add(a, b);
